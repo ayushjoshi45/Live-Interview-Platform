9972bcafeb4646d2501f0888d1ea6e36f6578e78
ayushjoshi45 <ayushjoshi3725@gmail.com>
2026-02-08 12:04:03 +0530

    deployment config setup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,13 +1,25 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import ENV from './lib/env.js';
+import path from 'path';
 const app =express();
-dotenv.config();
+const __dirname = path.resolve();
 
-console.log(process.env.key);
+
 app.get("/health",(req,res)=>{
     res.status(200).json({message:"This is the response from backend"});
 })
 
-app.listen(5000,()=>{
-    console.log("server running on port 5000");
+app.get("/books",(req,res)=>{
+    res.status(200).json({message:"This is the response from backend for books"});
+})
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend", "dist", "index.html"));
+    })
+}
+
+app.listen(ENV.PORT,()=>{
+    console.log(`Server Running on port ${ENV.PORT}`);
 })
